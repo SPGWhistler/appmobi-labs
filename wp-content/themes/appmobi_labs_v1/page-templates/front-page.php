80de6597c1fabc17c901defb9641ab2a80d25927
Tony Petty <tpetty@appmobi.com>
2013-05-02 11:55:59 -0400

    Working on front page.

--- wp-content/themes/appmobi_labs_v1/page-templates/front-page.php
+++ wp-content/themes/appmobi_labs_v1/page-templates/front-page.php
@@ -18,14 +18,15 @@ get_header(); ?>
 		<div id="content" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
-				<?php if ( has_post_thumbnail() ) : ?>
-					<div class="entry-page-image">
-						<?php the_post_thumbnail(); ?>
-					</div><!-- .entry-page-image -->
-				<?php endif; ?>
-
-				<?php get_template_part( 'content', 'page' ); ?>
-
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<div class="entry-content">
+						<?php the_content(); ?>
+						<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
+					</div><!-- .entry-content -->
+					<footer class="entry-meta">
+						<?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>
+					</footer><!-- .entry-meta -->
+				</article><!-- #post -->
 			<?php endwhile; // end of the loop. ?>
 
 		</div><!-- #content -->
